ef19c8836f0528f131699e23226dc56d40e341ff
amirmohammadmehri <mehri.developer@gmail.com>
2022-06-27 08:19:01 +0430

    change login.html and settings.py

--- uniform/settings.py
+++ uniform/settings.py
@@ -76,6 +76,16 @@ WSGI_APPLICATION = 'uniform.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -86,6 +96,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
 
 
 # Password validation
